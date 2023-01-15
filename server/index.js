0cdd15df91691484a39f8476512f56f467ec6014
me33x3 <me33x3@gmail.com>
2023-01-15 23:08:11 +0900

    mongodb 연결

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
+.then(() => console.log('Mongo DB Connected ...'))
+.catch((err) => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
