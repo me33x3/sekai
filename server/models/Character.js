6ed1822b3df4ca6f0cf7ac18f00d57e9257e3101
me33x3 <me33x3@gmail.com>
2023-01-21 01:46:49 +0900

    캐릭터 모델 변경

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,12 +1,58 @@
 const mongoose = require('mongoose')
 
 const characterSchema = mongoose.Schema({
-  chr_nm: {
+  chr_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  birthday: {
+    type: String,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  school: {
+    type: String
+  },
+  class: {
+    type: String
+  },
+  hobby: {
+    type: Array
+  },
+  specialty: {
+    type: Array
+  },
+  favorite_food: {
+    type: Array
+  },
+  disliked_food: {
+    type: Array
+  },
+  weakness: {
+    type: Array
+  },
+  sd: {
     type: String,
     required: true
   },
   grp_id: {
-    type: String,
+    type: Number,
     required: true
   }
 })
--- server/routes/Character.js
+++ server/routes/Character.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const Character = require('../models/Character')
 
 router.get("/", async (req, res) => {
-  const characters = await Character.find({})
+  const characters = await Character.find().sort({chr_id: 1})
   res.status(200).json(characters)
 });
 
