ee97f526ea4056b4a7d683c0241f064c0a2163f3
me33x3 <me33x3@gmail.com>
2023-02-03 18:05:24 +0900

    년월 변경 기능 구현

--- src/pages/Future.js
+++ src/pages/Future.js
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import moment from 'moment'
 import 'moment/locale/ko';
 
 const Future = () => {
   const mom = moment()
-  const year = mom.format('YYYY')
-  const month = mom.format('MM')
+  const today = [parseInt(mom.format('YYYY')), parseInt(mom.format('MM'))]
+
+  const [year, setYear] = useState(today[0])
+  const [month, setMonth] = useState(today[1])
+
+  const prevMonth = () => {
+    if (year === today[0] && month === today[1]) {
+      return
+    } else if (month === 1) {
+      setMonth(12)
+      setYear(year - 1)
+    } else {
+      setMonth(month - 1)
+    }
+  }
+
+  const nextMonth = () => {
+    if (month === 12) {
+      setMonth(1)
+      setYear(year + 1)
+    } else {
+      setMonth(month + 1)
+    }
+  }
 
   return (
     <div>
       <Container className='ftr-date-area'>
         <Row className="justify-content-md-center">
           <Col className='ftr-date-col' md='auto'>
-            <Button variant="light" size='sm'>&lt;</Button>
+            <Button variant="light" size='sm' onClick={() => prevMonth()}>&lt;</Button>
           </Col>
           <Col className='ftr-date-col' md={ 2 }>
             <div>
-              <p className='ftr-txt-month'>{ month }</p>
+              <p className='ftr-txt-month'>{ String(month).padStart(2, '0') }</p>
               <p className='ftr-txt-year'>{ year }</p>
             </div>
           </Col>
           <Col className='ftr-date-col' md='auto'>
-            <Button variant="light" size='sm'>&gt;</Button>
+            <Button variant="light" size='sm' onClick={() => nextMonth()}>&gt;</Button>
           </Col>
         </Row>
       </Container>
